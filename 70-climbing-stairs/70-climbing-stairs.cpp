ea4c7ba70fee3cc13e03bb4fd6946a61629d5746
jiyePark <parkji3618@naver.com>
2022-09-07 14:42:01 +0900

    Time: 5 ms (12.69%), Space: 6.1 MB (31.66%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climb(int n, int* stairs){
+        if(n<2) return 1;
+        if(stairs[n-1]==0) stairs[n-1]=climb(n-1, stairs);
+        if(stairs[n-2]==0) stairs[n-2]=climb(n-2, stairs);
+        
+        return stairs[n-1]+stairs[n-2];
+    }
+    int climbStairs(int n) {
+        int* stairs = (int*)malloc(sizeof(int)*n);
+        for(int i=0;i<n;i++){
+            stairs[i]=0;
+        }
+        return climb(n, stairs);
+    }
+};

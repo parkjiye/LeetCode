d23270960a5ca9acbdae3592bba32f2069e684ae
jiyePark <parkji3618@naver.com>
2022-09-13 23:09:44 +0900

    Time: 46 ms (19.88%), Space: 17 MB (6.20%) - LeetHub

---
+++ 136-single-number/136-single-number.c
@@ -0,0 +1,28 @@
+
+
+int singleNumber(int* nums, int numsSize){
+    int num = 3*10000;
+    int* arrays=malloc(sizeof(int)*num*2);
+    
+    for(int i=0;i<num;i++){
+        arrays[i]=0;
+    }
+    for(int i=0;i<numsSize;i++){
+        if(arrays[nums[i]+num]==0){
+            arrays[nums[i]+num]=60001;
+            continue;
+        }
+        if(arrays[nums[i]+num]==60001){
+            arrays[nums[i]+num]=nums[i];
+            continue;
+        }
+    }
+    int answ=0;
+    for(int i=0;i<num*2;i++){
+        if(arrays[i]==60001){
+            answ=i-30000;
+        }
+    }
+    
+    return answ;
+}

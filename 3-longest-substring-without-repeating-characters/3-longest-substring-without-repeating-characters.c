46b4196efe9e4fea576f1e45f126d79b9f374d3b
jiyePark <parkji3618@naver.com>
2022-10-02 18:53:37 +0900

    Time: 60 ms (21.59%), Space: 6 MB (43.44%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.c
@@ -0,0 +1,52 @@
+
+int check[128];
+
+int lengthOfLongestSubstring(char * s){
+    
+    int left = 0;
+    int count = 0;
+    int ans=0;
+    
+    if(strlen(s)==1)
+    {
+        return 1;
+    }
+    
+    for(int i=0;i<128;i++)
+    {
+        check[i]=0;
+    }
+    
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(check[s[i]]==0)
+        {
+            check[s[i]]=1;
+            count++;
+        }
+        else
+        {
+            for(;left<i;left++)
+            {
+                if(s[left]==s[i])
+                {
+                    printf("%d", count);
+                    left++;
+                    break;
+                }
+                else
+                {
+                    count--;
+                    check[s[left]]=0;
+                }
+            }
+        }
+        if(count>ans)
+        {
+            ans=count;
+        }
+            
+    }
+    
+    return ans;
+}

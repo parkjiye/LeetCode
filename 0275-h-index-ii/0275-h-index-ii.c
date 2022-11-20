605fb878d2c78849002c4b0e9452e42d3299c2e9
jiye Park <parkji3618@naver.com>
2022-11-20 20:52:05 +0900

    Time: 18 ms (100.00%), Space: 9.4 MB (40.63%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.c
@@ -0,0 +1,9 @@
+int hIndex(int* citations, int citationsSize){
+
+    for(int i=0;i<citationsSize;i++)
+    {
+        if(citations[i]>=citationsSize-i) return citationsSize-i;
+    }
+
+    return 0;
+}

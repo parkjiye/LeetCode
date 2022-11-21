cec159c2a8f7d80f3592cb13de6b3d0949633bca
jiye Park <parkji3618@naver.com>
2022-11-21 23:10:02 +0900

    Time: 34 ms (64.52%), Space: 9.3 MB (90.32%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.c
+++ 0275-h-index-ii/0275-h-index-ii.c
@@ -1,4 +1,4 @@
-int hIndex(int* citations, int citationsSize){
+/*int hIndex(int* citations, int citationsSize){
 
     for(int i=0;i<citationsSize;i++)
     {
@@ -6,4 +6,23 @@ int hIndex(int* citations, int citationsSize){
     }
 
     return 0;
+}*/
+
+int binary_search(int* citations, int l, int r, int n) {
+    int answer = 0;
+
+    while(l<=r) {
+        int mid = l+(r-l)/2;
+
+        if(citations[mid]>=n-mid) {
+            answer = n-mid;
+            r=mid-1;
+        }
+        else l=mid+1;
+    }
+
+    return answer;
+}
+int hIndex(int* citations, int citationsSize){
+    return binary_search(citations, 0, citationsSize-1, citationsSize);
 }

a5092635e42b3dcca96e152a602e78144cd6ac8d
jiye Park <parkji3618@naver.com>
2023-03-20 17:09:15 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (91.58%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int idx=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(idx==s.length()) return true;
+            else
+            {
+                if(s[idx]==t[i])
+                {
+                    idx++;
+                }
+            }
+        }
+
+        if(idx==s.length()) return true;
+        return false;
+    }
+};
